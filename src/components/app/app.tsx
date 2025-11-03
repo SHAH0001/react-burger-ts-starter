b8a5b7e62b7d68091360b377d9a22aec2530403a
Vlad <organizationshah@gmail.com>
2025-11-03 15:44:18 +0600

    Подключение к api

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,23 +1,40 @@
+import { Preloader } from '@krgaa/react-developer-burger-ui-components';
+import { useEffect, useState } from 'react';
+
 import { AppHeader } from '@components/app-header/app-header';
 import { BurgerConstructor } from '@components/burger-constructor/burger-constructor';
 import { BurgerIngredients } from '@components/burger-ingredients/burger-ingredients';
-import { ingredients } from '@utils/ingredients';
+
+import { serverUrl } from '../../utils/serverUrl';
 
 import styles from './app.module.css';
 
 export const App = (): React.JSX.Element => {
-  return (
-    <div className={styles.app}>
-      <AppHeader />
-      <h1 className={`${styles.title} text text_type_main-large mt-10 mb-5 pl-5`}>
-        Соберите бургер
-      </h1>
-      <main className={`${styles.main} pl-5 pr-5`}>
-        <BurgerIngredients ingredients={ingredients} />
-        <BurgerConstructor ingredients={ingredients} />
-      </main>
-    </div>
-  );
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    void fetch(`${serverUrl}ingredients`)
+      .then((response) => response.json())
+      .then((response) => setIngredients(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  if (ingredients.length === 0) {
+    return <Preloader />;
+  } else {
+    return (
+      <div className={styles.app}>
+        <AppHeader />
+        <h1 className={`${styles.title} text text_type_main-large mt-10 mb-5 pl-5`}>
+          Соберите бургер
+        </h1>
+        <main className={`${styles.main} pl-5 pr-5`}>
+          <BurgerIngredients ingredients={ingredients} />
+          <BurgerConstructor ingredients={ingredients} />
+        </main>
+      </div>
+    );
+  }
 };
 
 export default App;
