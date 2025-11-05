74c17b188af10a4f7784e98bd9f3b1fee275b801
Vlad <organizationshah@gmail.com>
2025-11-05 16:45:52 +0600

    Ингридиенты показаны по категориям

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,4 +1,5 @@
 import { Tab } from '@krgaa/react-developer-burger-ui-components';
+// import { useEffect, useState } from 'react';
 
 import { BurgerIngredient } from './burger-ingredient/burger-ingredient';
 
@@ -14,6 +15,10 @@ export const BurgerIngredients = ({
   ingredients,
 }: TBurgerIngredientsProps): React.JSX.Element => {
   console.log('BurgerIngredients: ', ingredients);
+
+  const buns = ingredients.filter((ingredient) => ingredient.type === 'bun');
+  const fillings = ingredients.filter((ingredient) => ingredient.type === 'main');
+  const sauces = ingredients.filter((ingredient) => ingredient.type === 'sauce');
 
   return (
     <section className={styles.burger_ingredients}>
@@ -48,10 +53,25 @@ export const BurgerIngredients = ({
           </Tab>
         </ul>
       </nav>
-      <div className={styles.ingredients}>
-        {ingredients.map((ingredient) => (
-          <BurgerIngredient key={ingredient._id} ingredient={ingredient} />
-        ))}
+      <div className={styles.container}>
+        <div className="text text_type_main-medium mt-10">Булки</div>
+        <div className={styles.ingredients}>
+          {buns.map((bun) => (
+            <BurgerIngredient key={bun._id} ingredient={bun} />
+          ))}
+        </div>
+        <div className="text text_type_main-medium">Начинки</div>
+        <div className={styles.ingredients}>
+          {fillings.map((filling) => (
+            <BurgerIngredient key={filling._id} ingredient={filling} />
+          ))}
+        </div>
+        <div className="text text_type_main-medium">Соусы</div>
+        <div className={styles.ingredients}>
+          {sauces.map((sauce) => (
+            <BurgerIngredient key={sauce._id} ingredient={sauce} />
+          ))}
+        </div>
       </div>
     </section>
   );
