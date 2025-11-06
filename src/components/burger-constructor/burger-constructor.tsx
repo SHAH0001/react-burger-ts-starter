4e1c1a220b5cf6eecc6dd9b0e61b0ba5d1627f32
Vlad <organizationshah@gmail.com>
2025-11-06 15:47:42 +0600

    Сверстан блок оформления заказа

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,4 +1,8 @@
-import { CurrencyIcon, LockIcon } from '@krgaa/react-developer-burger-ui-components';
+import {
+  CurrencyIcon,
+  LockIcon,
+  Button,
+} from '@krgaa/react-developer-burger-ui-components';
 
 import { BurgerConstructorItem } from './burger-constructor-item/burger-constructor-item';
 
@@ -37,8 +41,14 @@ export const BurgerConstructor = ({
         <CurrencyIcon type="primary" className="mr-5" />
         <LockIcon type="secondary" />
       </div>
-      <div className="mt-10">
-        <div className="text text_type_digits-medium">610</div>
+      <div className={`${styles.place_order} mt-10`}>
+        <div className={`${styles.price} mr-10`}>
+          <div className="text text_type_digits-medium mr-1">610</div>
+          <CurrencyIcon type="primary" />
+        </div>
+        <Button size="large" type="primary" htmlType={'button'}>
+          Оформить заказ
+        </Button>
       </div>
     </section>
   );
