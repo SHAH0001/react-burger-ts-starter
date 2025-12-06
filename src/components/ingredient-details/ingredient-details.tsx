d8009b2d7e55d74b5ee509264a2574c09e3db18b
Vlad <organizationshah@gmail.com>
2025-12-06 11:48:44 +0600

    При открытом модальном окне перезагрука страницы работает нормально

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,4 +1,7 @@
-import { useSelector } from 'react-redux';
+import { modalIngredient } from '@/services/ingredients/actions';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 import type { RootState } from '../../services/store';
 import type { TIngredient } from '@/utils/types';
@@ -6,32 +9,45 @@ import type { TIngredient } from '@/utils/types';
 import styles from './ingredient-details.module.css';
 
 export const IngredientDetails = (): React.JSX.Element => {
-  const modalIngredient = useSelector<RootState, TIngredient>(
+  const ingredient = useSelector<RootState, TIngredient>(
     (state): TIngredient => state.ingredients.modalIngredient as TIngredient
   );
-  console.log('modalIngredient: ', modalIngredient);
+
+  const ingredients = useSelector<RootState, TIngredient[]>(
+    (state): TIngredient[] => state.ingredients.ingredients as TIngredient[]
+  );
+  const { id } = useParams<'id'>();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const findIngredient = ingredients.find((ingredient) => ingredient._id === id);
+    if (!findIngredient) {
+      return;
+    }
+    dispatch(modalIngredient(findIngredient));
+  }, []);
   return (
     <div className={styles.modal}>
-      <img src={modalIngredient.image_large} alt={modalIngredient.name} />
-      <div className="text text_type_main-medium mt-4 mb-8">{modalIngredient.name}</div>
+      <img src={ingredient?.image_large} alt={ingredient?.name} />
+      <div className="text text_type_main-medium mt-4 mb-8">{ingredient?.name}</div>
       <div
         className={`${styles.nutritional_value} text text_type_main-default text_color_inactive`}
       >
         <div className="mr-5">
           <div>Калории,ккал</div>
-          <div className={styles.energy_value}>{modalIngredient.calories}</div>
+          <div className={styles.energy_value}>{ingredient?.calories}</div>
         </div>
         <div className="mr-5">
           <div>Белки, г</div>
-          <div className={styles.energy_value}>{modalIngredient.proteins}</div>
+          <div className={styles.energy_value}>{ingredient?.proteins}</div>
         </div>
         <div className="mr-5">
           <div>Жиры, г</div>
-          <div className={styles.energy_value}>{modalIngredient.fat}</div>
+          <div className={styles.energy_value}>{ingredient?.fat}</div>
         </div>
         <div>
           <div>Углеводы, г</div>
-          <div className={styles.energy_value}>{modalIngredient.carbohydrates}</div>
+          <div className={styles.energy_value}>{ingredient?.carbohydrates}</div>
         </div>
       </div>
     </div>
